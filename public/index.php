3af5e64ce385d7e8e38ca24e09c6e5bec9e9f349
fazza7777 <aungbarlaay7777@gmail.com>
2021-08-04 15:00:00 +0630

    end send mail with img

--- public/index.php
+++ public/index.php
@@ -13,7 +13,8 @@ $data = [
     "from_name"=>"Pyae Phyoe Naing",
     "subject"=>"Buy Now Shopping",
     "content" => $content,
-    "filename"=>"mail"
+    "filename"=>"mail",
+    "img_link"=> "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlPsFp6x3bSGfAwzw9jxCGPLBG2ZQOmXIJLQ&usqp=CAU"
 ];
 
 $cond = $mail->send($data);
--- resources/views/mails/mail.blade.php
+++ resources/views/mails/mail.blade.php
@@ -6,17 +6,25 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Buy Now</title>
     <style>
-        h1{
+        h4{
             color:white;
             background: rgb(248, 36, 160);
             line-height: 25px;
             text-align: center;
             margin-bottom: 30px;
         }
+        .end{
+            background: rgb(13, 207, 101);
+            color:white;
+            padding: 20px 0;
+            text-align: center
+        }
     </style>
 </head>
 <body>
-    <h1>Welcome Buy Now Shopping</h1>
+    <h4>Welcome Buy Now Shopping</h4>
     <p><?php echo $content; ?></p>
+    <img src="<?php echo $img_link ?>" alt="">
+    <p class="end"><?php echo $subject; ?></p>
 </body>
 </html>
