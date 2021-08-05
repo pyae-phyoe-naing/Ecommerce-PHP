bc9d2da049d499315e5964c22bd73a42d03eea94
fazza7777 <aungbarlaay7777@gmail.com>
2021-08-05 19:25:27 +0630

    define errors message and set,get,check

--- app/classes/ValidateRequest.php
+++ app/classes/ValidateRequest.php
@@ -11,8 +11,20 @@ class ValidateRequest
      * @param $value   // Column Value
      * @param $policy  // Dynamic => Table name , string count etc..
      */
+    private $errors = [];
+    private $error_message = [
+        'unique'=>':attribute field is already exist',
+        'required'=>':attribute field is required',
+        'minLength'=>':attribute field must be at least :policy characters',
+        'maxLength'=>':attribute field must be most :policy characters',
+        'email'=>':attribute field must be a valid email',
+        'string'=>':attribute field must be string value',
+        'number'=>':attribute field must be number value',
+        'mixed'=>':attribute field allows [ A-Z , a-z , . , $ , @ ] characters',
 
-   public function unique($column,$value,$policy){
+    ];
+
+    public function unique($column,$value,$policy){
       if($value != null && !empty(trim($value))){
           return Capsule::table($policy)->where($column,$value)->exists();
       }
@@ -27,6 +39,7 @@ class ValidateRequest
             return strlen(trim($value)) >= $policy;
         }
     }
+
     public function maxLength($column,$value,$policy){
         if($value != null && !empty(trim($value))){
             return strlen(trim($value)) <= $policy;
@@ -39,22 +52,42 @@ class ValidateRequest
         }
         return  false;
     }
+
     public function string($column,$value,$policy){
         if($value != null && !empty(trim($value))){
             return preg_match("/^[A-za-z]+$/",$value);
         }
         return  false;
     }
+
     public function number($column,$value,$policy){
         if($value != null && !empty(trim($value))){
-            return preg_match("/^[0-9]+$/",$value);
+            return preg_match("/^[0-9\.]+$/",$value);
         }
         return  false;
     }
+
     public function mixed($column,$value,$policy){
         if($value != null && !empty(trim($value))){
             return preg_match("/^[A-za-z0-9 \.$@]+$/",$value);
         }
         return  false;
+    }
+
+    public  function setError($error,$key=null){
+        if($key){
+            $this->errors[$key] = $error;
+        }else{
+            $this->errors[] = $error;
+        }
+
+    }
+
+    public function hasError(){
+        return count($this->errors) > 0 ? true : false;
+    }
+
+    public function getError(){
+        return $this->errors;
     }
 }
